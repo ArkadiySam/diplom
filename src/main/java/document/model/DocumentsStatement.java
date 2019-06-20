fd9be3d73dcd4a6feb966ad7db59217d4ae4b3f7
Arkadiy <Arkadiy>
2019-06-20 12:14:41 +0600

    реализация 5 документов

--- src/main/java/document/model/DocumentsStatement.java
+++ src/main/java/document/model/DocumentsStatement.java
@@ -4,6 +4,7 @@ import document.Document;
 import tools.ElementType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DocumentsStatement extends Document {
     private static final String path = "vedomost_expluatatsionnykh_dokumentov.rtf";
@@ -14,6 +15,15 @@ public class DocumentsStatement extends Document {
         elements.add(new ElementType("number", "Количество листов", "count"));
         elements.add(new ElementType("text", "Год", "year"));
         elements.add(new ElementType("textarea", "Аннотация", "annotation"));
+        elements.add(new ElementType("table", "", "noName",
+                new ArrayList<>(Arrays.asList("Обозначение", "Наименование","Кол. экз",
+                        "Местонахождение")), 8));
+        elements.add(new ElementType("table", "Лист регистрации изменений", "register",
+                new ArrayList<>(Arrays.asList("Изм.", "изменен-ных",
+                        "заменен-ных", "новых","аннули-рованных",
+                        "Всего листов (страниц) в докум",
+                        "№ докумен-та","Входящий № сопроводит. докум. и дата",
+                        "Подп", "Дата")), 20));
     }
 
     public DocumentsStatement () {
--- src/main/java/document/model/OriginalsStatement.java
+++ src/main/java/document/model/OriginalsStatement.java
@@ -4,6 +4,7 @@ import document.Document;
 import tools.ElementType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OriginalsStatement extends Document {
     private static final String path = "vedomost_derzhateley_podlinnikov.rtf";
@@ -13,6 +14,15 @@ public class OriginalsStatement extends Document {
         elements.add(new ElementType("text", "Имя документа", "title"));
         elements.add(new ElementType("number", "Количество листов", "count"));
         elements.add(new ElementType("textarea", "Аннотация", "annotation"));
+        elements.add(new ElementType("table", "", "noName",
+                new ArrayList<>(Arrays.asList("Обозначение", "Наименование","Держатель подлинника",
+                        "Примечание")), 8));
+        elements.add(new ElementType("table", "Лист регистрации изменений", "register",
+                new ArrayList<>(Arrays.asList("Изм.", "изменен-ных",
+                        "заменен-ных", "новых","аннули-рованных",
+                        "Всего листов (страниц) в докум",
+                        "№ докумен-та","Входящий № сопроводит. докум. и дата",
+                        "Подп", "Дата")), 20));
     }
 
     public OriginalsStatement() {
